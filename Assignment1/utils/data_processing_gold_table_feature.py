b7a444ad42aed3f20a3caa9c0a2de707290a83f6
Pauline Ng <paulineng@Paulines-MacBook-Pro.local>
2025-05-13 03:51:18 +0800

    add gold

--- Assignment1/utils/data_processing_gold_table_feature.py
+++ Assignment1/utils/data_processing_gold_table_feature.py
@@ -15,30 +15,40 @@ from pyspark.sql.functions import col
 from pyspark.sql.types import StringType, IntegerType, FloatType, DateType
 
 
-def process_labels_gold_table(snapshot_date_str, silver_loan_daily_directory, gold_label_store_directory, spark, dpd, mob):
+def process_labels_gold_table(snapshot_date_str, silver_clickstream_directory, silver_attributes_directory, silver_financials_directory, gold_feature_store_directory, spark):
     
     # prepare arguments
     snapshot_date = datetime.strptime(snapshot_date_str, "%Y-%m-%d")
-    
-    # connect to bronze table
-    partition_name = "silver_loan_daily_" + snapshot_date_str.replace('-','_') + '.parquet'
-    filepath = silver_loan_daily_directory + partition_name
-    df = spark.read.parquet(filepath)
-    print('loaded from:', filepath, 'row count:', df.count())
 
-    # get customer at mob
-    df = df.filter(col("mob") == mob)
+    # connect to silver clickstream 
+    part = snapshot_date_str.replace('-', '_')
+    partition_name = f"silver_feature_clickstream_{part}.parquet"
+    filepath = silver_clickstream_directory + partition_name
+    df_click = spark.read.parquet(filepath)
+    print(f"loaded clickstream from: {filepath}, rows: {df_click.count():,}")
 
-    # get label
-    df = df.withColumn("label", F.when(col("dpd") >= dpd, 1).otherwise(0).cast(IntegerType()))
-    df = df.withColumn("label_def", F.lit(str(dpd)+'dpd_'+str(mob)+'mob').cast(StringType()))
+    # connect to silver attributes 
+    partition_name = f"silver_features_attributes_{part}.parquet"
+    filepath = silver_attributes_directory + partition_name
+    df_attr = spark.read.parquet(filepath)
+    print(f"loaded attributes   from: {filepath}, rows: {df_attr.count():,}")
 
-    # select columns to save
-    df = df.select("loan_id", "Customer_ID", "label", "label_def", "snapshot_date")
+    # connect to silver financials 
+    partition_name = f"silver_feature_financials_{part}.parquet"
+    filepath = silver_financials_directory + partition_name
+    df_fin = spark.read.parquet(filepath)
+    print(f"loaded financials   from: {filepath}, rows: {df_fin.count():,}")
+
+    # now full‐outer‐join
+    df = (
+        df_click
+          .join(df_fin, on=["Customer_ID","snapshot_date"], how="outer")
+          .join(df_attr, on=["Customer_ID","snapshot_date"], how="outer")
+    )
 
     # save gold table - IRL connect to database to write
-    partition_name = "gold_label_store_" + snapshot_date_str.replace('-','_') + '.parquet'
-    filepath = gold_label_store_directory + partition_name
+    partition_name = "gold_feature_store_" + snapshot_date_str.replace('-','_') + '.parquet'
+    filepath = gold_feature_store_directory + partition_name
     df.write.mode("overwrite").parquet(filepath)
     # df.toPandas().to_parquet(filepath,
     #           compression='gzip')
